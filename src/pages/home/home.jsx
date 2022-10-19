9d625b7b7319fd7cd27d7073a04a9e45ea40a5e3
Isabel Queiroz <belqrdev@outlook.com>
2022-10-19 19:15:21 -0300

    feat: created component 'home' and stylized

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,7 +1,9 @@
+import { Container } from './style';
 import Photo from '../../assets/images/circle-photo.png';
 import Spot01 from '../../assets/images/spot-green1.png';
 import Line from '../../assets/images/line-green.png';
-import { Container } from './style';
+import Spot02 from '../../assets/images/spot-green2.png'
+import Line1 from '../../assets/images/line-1.png';
 
 function Home() {
    return(
@@ -11,11 +13,14 @@ function Home() {
          </div>
          <div>
             <p> Olá, eu sou a </p>
-            <p className='name'> ISABEL QUEIROZ, | </p>
+            <p className='name'> ISABEL QUEIROZ,| </p>
             <p> desenvolvedora front-end </p>
          </div>
          <img src={Spot01} alt='Spot' className='spot'/>
          <img src={Line} alt='Line' className='line'/>
+
+         <img src={Spot02} alt='Spot' className='spot-mob'/>
+         <img src={Line1} alt='Line' className='line-mob'/>
       </Container>
    )
 }
--- src/pages/home/style.js
+++ src/pages/home/style.js
@@ -4,29 +4,36 @@ export const Container = styled.div`
    width: 100%;
    display: flex;
    align-items: center;
+   justify-content: center;
 
    div:first-child {
-      border: 2px solid red;
+      margin-right: 10px;
+      
+      >img {
+         width: 100%;
+      }
    }
 
    >div {
-      border: 2px solid purple;
       position: relative;
       z-index: 999;
+      text-align: center;
+      
 
       p {
-         font-size: 30px;
+         font-size: 2rem;
+         margin: 5px 0;
       }
 
       .name {
          font-family: 'Press Start 2P', cursive;
-         font-size: 40px;
+         font-size: 2.8rem;
       }
    }
 
    .spot {
       width: 100%;
-      max-width: 650px;
+      max-width: 35%;
       position: absolute;
       top: 0;
       right: 0;
@@ -35,9 +42,65 @@ export const Container = styled.div`
 
    .line {
       width: 100%;
-      max-width: 350px;
+      max-width: 30%;
       position: absolute;
       bottom: 0;
       right: 0;
    }
+
+   .spot-mob {
+      display: none;
+   }
+
+   .line-mob {
+      display: none;
+   }
+
+   @media (max-width: 375px) {
+      flex-direction: column;
+      justify-content: flex-start;
+
+      div:first-child {
+      margin-right: 0;
+      margin-top: 75px;
+      
+      img {
+         width: 80%;
+      }
+    }
+
+   >div {
+
+      p {
+         font-size: 1rem;
+      }
+
+      .name {
+         font-size: 1.25rem;
+      }
+   }
+
+   .spot {
+      display: none;
+   }
+
+   .line {
+      display: none;
+   }
+
+   .spot-mob {
+      display: inline-block;
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      z-index: 9999;
+   }
+
+   .line-mob {
+      display: inline-block;
+      position: absolute;
+      bottom: 0;
+      left: 0;
+   }
+}
 `

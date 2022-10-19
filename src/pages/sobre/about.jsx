96e1da66677c511fd4e16a35a008302968985f37
Isabel Queiroz <belqrdev@outlook.com>
2022-10-19 19:15:47 -0300

    feat: created component 'about' and stylized

--- src/pages/sobre/about.jsx
+++ src/pages/sobre/about.jsx
@@ -1,8 +1,17 @@
+import Me from './components/me/me';
+import Animals from './components/motivation/animals';
+import Projects from './components/projects/project';
+import Spot3 from '../../assets/images/spot-green3.png';
+import { Container } from './style';
+
 function About() {
    return(
-      <>
-         <h1> About </h1>
-      </>
+      <Container>
+         <Me />
+         <Animals />
+         <Projects />
+         <img src={Spot3} alt='Spot' className='spot3' />
+      </Container>
    )
 }
 export default About;
---
+++ src/pages/sobre/style.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components' ;
+
+export const Container = styled.div`
+   width: 100%;
+   display: flex;
+   flex-direction: column;
+
+   .spot3 {
+      width: 100%;
+      max-width: 25%;
+      position: absolute;
+      bottom: 0;
+      right: 0;
+      z-index: -999;
+   }
+`

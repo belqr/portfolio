e1e761dfe8279e997afe4c6e210ad2005d2e8e00
Isabel Queiroz <belqrdev@outlook.com>
2022-10-21 22:02:33 -0300

    feat:  created component and stylized

--- src/pages/contato/contact.jsx
+++ src/pages/contato/contact.jsx
@@ -1,9 +1,25 @@
+import Form from './components/form';
+import { Container } from './style';
 
 function Contact() {
    return(
-      <>
-         <h1> contato </h1>
-      </>
+      <Container>
+         <h2> Contato </h2>
+         <section>
+            <div className='box-contact-me'>
+               <div>
+                  <i> <h3> Quer falar comigo? </h3> </i>
+                  <p> Me mande uma mensagem! </p>
+               </div>
+               <div>
+                  <p> +55 (73) 9 9965-7784 </p>
+                  <p> belqrdev@outlook.com </p>
+                  <a href=''> <p> Curriculum </p> </a>
+               </div>
+            </div>
+            <Form />
+         </section>
+      </Container>
    )
 }
 export default Contact;
---
+++ src/pages/contato/style.js
@@ -0,0 +1,122 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+   margin: 50px 0;
+   width: 100%;
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+
+   h2 {
+      margin-bottom: 50px;
+      padding: 10px;
+      width: fit-content;
+      text-align: center;
+      font-size: 2.5rem;
+      font-weight: 500;
+      font-family: 'Concert One', cursive;
+      border-bottom: 6px solid #C2E198;
+   }
+
+   section {
+      width: 85%;
+      display: flex;
+      justify-content: space-evenly;
+
+      .box-contact-me {
+         width: 100%;
+         display: flex;
+         flex-direction: column;
+
+         div:first-child {
+            margin: 25px 0;
+            width: 100%;
+            display: flex;
+            flex-direction: column;
+
+            h3 {
+               font-size: 26px;
+               font-weight: 600;
+            }
+   
+            p {
+               font-size: 22px;
+            }
+         }
+
+         div:last-child {
+            margin: 25px 0;
+            padding-left: 10px;
+            width: 100%;
+            display: flex;
+            flex-direction: column;
+            border-left: 3px solid #C2E198;
+
+            a {
+               text-decoration: none;
+            }
+
+            p {
+               padding: 5px 0;
+               font-size: 20px;
+            }
+         }
+      }
+   }
+
+   @media (max-width: 768px) {
+      margin: 25px 0;
+
+      h2 {
+         margin-bottom: 25px;
+         font-size: 1.75rem;
+         border-bottom: 4px solid #C2E198;
+      }
+
+      section {
+         width: 85%;
+         display: flex;
+         flex-direction: column;
+         justify-content: space-evenly;
+
+         .box-contact-me {
+            width: 100%;
+            display: flex;
+            flex-direction: column;
+
+            div:first-child {
+               margin: 25px 0;
+               width: 100%;
+               display: flex;
+               flex-direction: column;
+
+               h3 {
+                  font-size: 1.3rem;
+               }
+      
+               p {
+                  font-size: 20px;
+               }
+            }
+
+            div:last-child {
+               margin: 25px 0;
+               padding-left: 10px;
+               width: 100%;
+               display: flex;
+               flex-direction: column;
+               border-left: 3px solid #C2E198;
+
+               a {
+                  text-decoration: none;
+               }
+
+               p {
+                  padding: 5px 0;
+                  font-size: 16px;
+               }
+            }
+         }
+      }   
+   }
+`

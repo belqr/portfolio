1caf869cfeff261547f21d40eb933dd5b82f3e4d
Isabel Queiroz <belqrdev@outlook.com>
2022-10-24 22:03:31 -0300

    feat: fix component social

--- src/pages/social/social.jsx
+++ src/pages/social/social.jsx
@@ -1,11 +1,42 @@
+import { useState } from 'react';
 import Block01 from '../../assets/images/blocks/block-1.png';
 import Block02 from '../../assets/images/blocks/block-2.png';
 import Block03 from '../../assets/images/blocks/block-3.png';
 import Block04 from '../../assets/images/blocks/block-4.png';
 import Block05 from '../../assets/images/blocks/block-5.png';
+import ModalComponent from './components/modal/modal';
 import { Container } from './style';
 
+
 function Social() {
+   const [isOpen, setIsOpen] = useState({
+      open: false,
+      title: '',
+      txt: '',
+   });
+
+   const List = [
+      {
+         src: Block01, alt: 'Vida Terrestre', title: 'Vida Terrestre', txt: 'Proteger, restaurar e promover o uso sustentável dos ecossistemas terrestres, gerir de forma sustentável as florestas, combater a desertificação, travar e reverter a degradação dos solos e travar a perda da biodiversidade'
+      },
+      {
+         src: Block02, alt: 'Vida Animal', title: 'Vida Animal', txt: 'Adotar medidas urgentes para combater as alterações climáticas e os seus impactos' 
+      },
+      {
+         src: Block03, alt: 'Vida Áquatica', title: 'Vida Áquatica', txt: 'Conservar e usar de forma sustentável os oceanos, mares e os recursos marinhos para o desenvolvimento sustentável'
+      },
+      {
+         src: Block04, alt: 'Lazer', title: 'Lazer', txt: 'Garantir o acesso à saúde de qualidade e promover o bem-estar para todos, em todas as idades'
+      },
+      {
+         src: Block05, alt: 'Educação Sexual', title: 'Educação Sexual', txt: 'Garantir o acesso à educação inclusiva, de qualidade e equitativa, e promover oportunidades de aprendizagem ao longo da vida para todos'
+      },
+   ]
+
+   function openModal(title, txt) {
+      setIsOpen({open: true, title: title, txt: txt});
+    }
+
    return(
       <Container>
          <h2> OBJETIVOS DE DESENVOLVIMENTO SUSTENTÁVEL: </h2>
@@ -15,11 +46,12 @@ function Social() {
             </p>
          </div>
          <section>
-            <img src={Block01} alt='Vida Terrestre' />
-            <img src={Block02} alt='Vida Animal' />
-            <img src={Block03} alt='Vida Áquatica' />
-            <img src={Block04} alt='Lazer' />
-            <img src={Block05} alt='Educação Sexual' />
+            {
+               List.map(item => (
+                  <img onClick={() => openModal(item.title, item.txt)} src={item.src} alt={item.alt} />
+               ))
+            }
+            <ModalComponent isOpen={isOpen} setIsOpen={setIsOpen} title={isOpen.title} txt={isOpen.txt} />
          </section>
       </Container>
    )

a7489f1b6848913d0a3a21f8fa7f77fd0c70f0a2
Isabel Queiroz <belqrdev@outlook.com>
2022-10-26 18:52:33 -0300

    feat: created state in component

--- src/pages/contato/components/form.jsx
+++ src/pages/contato/components/form.jsx
@@ -1,15 +1,37 @@
+import { useState } from 'react';
 import { Container } from './style';
 
 function Form() {
+   const [name, setName] = useState('');
+   const [email, setEmail] = useState('');
+   const [message, setMessage] = useState('');
+
+
+   const changeName = (e) => {
+      setName(e.target.value);
+   }
+
+   const changeEmail = (e) => {
+      setEmail(e.target.value);
+   }
+
+   const changeMessage = (e) => {
+      setMessage(e.target.value);
+   }
+
+   const Submit = () => {
+      alert(`Enviar a '${message}' ?`)
+   }
+
    return(
       <Container>
          <form>
             <i> <h3> Contate me </h3> </i>
-            <input type='name' placeholder='nome' />
-            <input type='email'placeholder='email' />
-            <input type='text' placeholder='mensagem'/>
+            <input value={name} onChange={changeName} type='name' placeholder='nome' />
+            <input value={email} onChange={changeEmail} type='email'placeholder='email' />
+            <input value={message} onChange={changeMessage} type='text' placeholder='mensagem'/>
          </form>
-            <button> Envie um email </button>
+            <button onClick={Submit}> Envie um email </button>
       </Container>
    )
 }
